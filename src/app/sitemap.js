2a0c4e3f8f60a8bdb0d999efda2f9f8760bd728f
fariedzulkrn <fariedanjiezter@gmail.com>
2026-09-07 12:20:13 +0700

    Add sitemap

---
+++ src/app/sitemap.js
@@ -0,0 +1,15 @@
+export default function sitemap() {
+  const baseUrl = "https://darululumxvi.sch.id";
+
+  return [
+    { url: baseUrl },
+    { url: `${baseUrl}/profil` },
+    { url: `${baseUrl}/galeri` },
+    { url: `${baseUrl}/ppdb` },
+    { url: `${baseUrl}/spmb` },
+    { url: `${baseUrl}/kb` },
+    { url: `${baseUrl}/ra` },
+    { url: `${baseUrl}/mi` },
+    { url: `${baseUrl}/mdta` },
+  ];
+}
